3548936ba2985d4dfc6bf5dcf5fa2fb96fe8b95d
Reemoh <iamreemoh@gmail.com>
2022-12-14 12:24:17 +0100

    only L*A multiplication is left

--- trying permutation for row.py
+++ trying permutation for row.py
@@ -1,12 +1,12 @@
 import numpy as np
-A = np.array([[10,2,3,4,3,2,5,0],
-            [5,6,7,8,4,20,4,4],
-            [9,3,2,1,4,1,2,30],
-            [5,6,7,8,4,60,4,6],
-            [9,3,2,1,4,1,2,80],
-            [4,1,3,40,5,6,8,0],
-            [5,5,1,3,6,7,6,50],
-            [5,4,7,8,4,70,4,0]])
+A = np.array([[1,2,3,4,5,6,7,8],
+            [9,10,11,12,13,14,15,16],
+            [17,18,19,20,21,22,23,24],
+            [25,26,27,28,29,30,31,32],
+            [33,34,35,36,37,38,39,40],
+            [41,42,43,44,45,46,47,48],
+            [45,46,47,48,49,50,51,52],
+            [53,54,55,56,57,58,59,60]])
 
 def maxabs_idx(A):
     # TODO
@@ -15,8 +15,7 @@ def maxabs_idx(A):
     return (int(i),int(j))
 
 m,n=A.shape
-#print(m,n)
-U=A.copy()
+
 
 def lu_complete(A):
     #TODO
@@ -25,45 +24,32 @@ def lu_complete(A):
     L=np.identity(m, dtype=np.float32)
     P=np.identity(m)
     Z=np.zeros((m,m),dtype=np.float32)
-    Q = None
+    Q = np.identity(n)
 
-    U=A.copy()                 # U for copy of A
-    U.astype(np.float32)
+    _A_=A.copy()                 # _A_ for copy of A
+    _A_.astype(np.float32)
 
-    V=A.copy()                 # U for copy of A
+    V=A.copy()                 # _A_ for copy of A
     V.astype(np.float32)
 
     for k in range(m-1):
-        imax,jmax= maxabs_idx(U[k:m,k:n])
+        imax,jmax= maxabs_idx(_A_[k:m,k:n])
         #imax,jmax= maxabs_idx(V[k:m,k:n])
 
-        #--------------------OOPERATION ON U-----------------------------------
+        #--------------------OOPERATION ON _A_-----------------------------------
         # Interchanging Rows
-        temp=U[k,:].copy()
-        U[k,:]=U[imax+k,:].copy()
-        U[imax+k,:]=temp.copy()
+        temp=_A_[k,:].copy()
+        _A_[k,:]=_A_[imax+k,:].copy()
+        _A_[imax+k,:]=temp.copy()
 
-        if len(U[k+1:imax,:])!=0:
-            U[k+1:imax,:]=U[k+1:imax,:].copy()
-        if len(U[imax+1:m+1,:])!=0:
-            U[imax+1:m+1,:]=U[imax+1:m+1,:].copy()
+        if len(_A_[k+1:imax,:])!=0:
+            _A_[k+1:imax,:]=_A_[k+1:imax,:].copy()
+        if len(_A_[imax+1:m+1,:])!=0:
+            _A_[imax+1:m+1,:]=_A_[imax+1:m+1,:].copy()
 
-        U=U.copy()
-
-        temp=V[:,k].copy()
-
-        V[:,k]=V[:,jmax+k].copy()
-        V[:,jmax+k]=temp.copy()
-
-        if len(V[:,k+1:jmax])!=0:
-            V[:,k+1:jmax]=V[:,k+1:jmax].copy()
-        if len(V[:,jmax+1:n+1])!=0:
-            V[:,jmax+1:n+1]=V[:,jmax+1:n+1].copy()
-
-
-        V=V.copy()
+        _A_=_A_.copy()
         
-        #---------------^^^^^^^----OOPERATION ON U-------------^^^^^^^^^------------------
+        #---------------^^^^^^^----OOPERATION ON _A_-------------^^^^^^^^^------------------
 
         #------------------------- OPERATION ON P----------------------------------------
         temp=P[k,:].copy()
@@ -77,32 +63,49 @@ def lu_complete(A):
         #----------------^^^^^^--- OPERATION ON P---------------^^^^^^^^^------------------
         
         #------------------------- OPERATION ON L----------------------------------------
-        Z[k+1:,k]=np.add(Z[k+1:,k],U[k+1:,k])
+        Z[k+1:,k]=np.add(Z[k+1:,k],_A_[k+1:,k])
 
-        #print("iteration ",k,", Z[k+1:,k] =",Z[k+1:,k],"  U[k,k]  =",U[k,k])
-        L[k+1:,k]=np.divide(Z[k+1:,k],U[k,k])
+        #print("iteration ",k,", Z[k+1:,k] =",Z[k+1:,k],"  _A_[k,k]  =",_A_[k,k])
+        L[k+1:,k]=np.divide(Z[k+1:,k],_A_[k,k])
 
 
-        #---------------^^^^^^----OPERATION ON U------^^^^^^^^^----------------------------
-    print(U)
-    print("\n")
+        #---------------------------OPERATION ON _A_-----------------------------------------
+    #print(_A_)
+    #print("\n")
     
     for k in range(m-1):
-        imax,jmax= maxabs_idx(U[k:m,k:n])
+        imax,jmax= maxabs_idx(_A_[k:m,k:n])
 
-        temp=U[:,k].copy()
+        temp=_A_[:,k].copy()
 
-        U[:,k]=U[:,jmax+k].copy()
-        U[:,jmax+k]=temp.copy()
+        _A_[:,k]=_A_[:,jmax+k].copy()
+        _A_[:,jmax+k]=temp.copy()
 
-        if len(U[:,k+1:jmax])!=0:
-            U[:,k+1:jmax]=U[:,k+1:jmax].copy()
-        if len(U[:,jmax+1:n+1])!=0:
-            U[:,jmax+1:n+1]=U[:,jmax+1:n+1].copy()
+        if len(_A_[:,k+1:jmax])!=0:
+            _A_[:,k+1:jmax]=_A_[:,k+1:jmax].copy()
+        if len(_A_[:,jmax+1:n+1])!=0:
+            _A_[:,jmax+1:n+1]=_A_[:,jmax+1:n+1].copy()
 
+        _A_=_A_.copy()
+        #--------------------^^^^^^^--OPERATION ON _A_---^^^^^^^^------------------------------
+        #-----------------------------OPERATION ON Q-----------------------------------------
 
-        U=U.copy()
-    print(U)
+        temp=Q[:,k].copy()
+
+        Q[:,k]=Q[:,jmax+k].copy()
+        Q[:,jmax+k]=temp.copy()
+
+        if len(Q[:,k+1:jmax])!=0:
+            Q[:,k+1:jmax]=Q[:,k+1:jmax].copy()
+        if len(Q[:,jmax+1:n+1])!=0:
+            Q[:,jmax+1:n+1]=Q[:,jmax+1:n+1].copy()
+
+    print(L)
+    print("\n")
+    print(_A_)
+    print("\n")
+    U=np.multiply(L,_A_)
+    print(A)
 
     return (P, Q, L, U)
 lu_complete(A)
